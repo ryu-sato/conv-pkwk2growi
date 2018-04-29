bf938b428815d75f125cb4e266734df2a4a2317b
Ryu Sato <ryu@weseek.co.jp>
2018-04-30 01:18:02 +0900

    clean code

--- dump-pkwk.sh
+++ dump-pkwk.sh
@@ -2,9 +2,10 @@
 
 PAGE_FILE_NAME="page.txt"
 ATTACHMENTS_DIR_NAME="attachments"
-DUMP_DIR="/usr/local/src/conv-pkwk2growi/dump"
 
-# parent of /wiki directory
+# variables
+# [TODO] enable to set from command line
+DUMP_DIR="/usr/local/src/conv-pkwk2growi/dump"
 PUKIWIKI_DATADIR="/var/www/html"
 PUKIWIKI_PAGE_DIR="${PUKIWIKI_DATADIR}/wiki"
 PUKIWIKI_ATTACHMENTS_DIR="${PUKIWIKI_DATADIR}/attach"
