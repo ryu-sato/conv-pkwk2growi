1a026782335c9a77bb6b0168b9123315a5d4423c
Ryu Sato <ryu@weseek.co.jp>
2018-04-30 02:34:48 +0900

    enable to convert

--- converter.sh
+++ converter.sh
@@ -1,3 +1,19 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-sed -e 's/ \[#[0-9a-z]\+\]$//g' -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' -e 's/^---/        -/g' -e 's/^--/    -/g' -e 's/^\(\s*\)-\([^ ]\)/\1- \2/g' -e 's/&br;/<br>/g' -e 's/^#pre{*/```/g' -e 's/^}}*/```/g' -e 's/%%/~~/g' -e "s/^\#lsx/`echo -ne '\u0024'`lsx()/g"
+PAGE_FILE_NAME="page.txt"
+ATTACHMENTS_DIR_NAME="attachments"
+
+# variables
+# [TODO] enable to set from command line
+DUMP_DIR="/mnt/d/usr/DevApp/repos_github/ryu-sato/conv-pkwk2growi/dump"
+
+# convert pukiwiki pages to markdown
+cd ${DUMP_DIR}
+for f in `find . -name ${PAGE_FILE_NAME}`; do
+
+  # copy page directory
+  TEMPFILE=$(mktemp -t conv-pkwk2growi.XXXXX) || exit 1
+  # see. https://qiita.com/yuki-takei/items/152e20f4421333ae8fd9
+  cat ${f} | sed -e 's/ \[#[0-9a-z]\+\]$//g' -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' -e 's/^---/        -/g' -e 's/^--/    -/g' -e 's/^\(\s*\)-\([^ ]\)/\1- \2/g' -e 's/&br;/<br>/g' -e 's/^#pre{*/```/g' -e 's/^}}*/```/g' -e 's/%%/~~/g' -e "s/^\#lsx/`echo -ne '\u0024'`lsx()/g" > ${TEMPFILE}
+  mv $TEMPFILE ${f}
+done
