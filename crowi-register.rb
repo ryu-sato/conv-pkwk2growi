16f4ef860c1f17440260c080e438a64010d02a2f
Ryu Sato <ryu@weseek.co.jp>
2018-04-30 01:40:08 +0900

    enable to register crowi page with crowi-client.

--- crowi-register.rb
+++ crowi-register.rb
@@ -1,2 +1,12 @@
 require 'crowi-client'
 
+page_name = '/test/hoge'
+
+if CrowiClient.instance.page_exist?(path_exp: page_name)
+  raise "Cannot create page. Because page '#{page_name}' is already exists. You need to delete before register."
+end
+
+# Create page and attachment files
+body = STDIN.read
+req = CPApiRequestPagesCreate.new path: page_name, body: body
+puts CrowiClient.instance.request(req)
