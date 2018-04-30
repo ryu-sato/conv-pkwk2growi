abd2d990e09696498a00b3361f5cd730683e9046
Ryu Sato <ryu@weseek.co.jp>
2018-04-30 21:15:48 +0900

    # の後ろに空白を追加。

--- conv-pkwk2md.sh
+++ conv-pkwk2md.sh
@@ -11,7 +11,7 @@ for f in `find . -name ${PAGE_FILE_NAME}`; do
   TEMPFILE=$(mktemp -t conv-pkwk2growi.XXXXX) || exit 1
   # see. https://qiita.com/yuki-takei/items/152e20f4421333ae8fd9
   cat ${f} | sed -e 's/ \[#[0-9a-z]\+\]$//g' \
-    -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' \
+    -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' -e 's/^\(#\+\)\([^ #]\)/\1 \2/g' \
     -e 's/^---/        -/g' -e 's/^--/    -/g' -e 's/^\(\s*\)-\([^ ]\)/\1- \2/g' \
     -e 's/^[\+][\+][\+]/        1./g' -e 's/^[\+][\+]/    1./g' -e 's/^[\+]/1./g' -e 's/^\(\s*\)1\.\([^ ]\)/\11. \2/g' \
     -e 's/&br;/<br>/g' \
