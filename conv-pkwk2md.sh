a2bd86b894427fbee136529808d1e0f44a612db8
Ryu Sato <ryu@weseek.co.jp>
2018-04-30 20:48:25 +0900

    可読性を上げるために改行追加。番号付きリストに対応。

--- conv-pkwk2md.sh
+++ conv-pkwk2md.sh
@@ -10,6 +10,14 @@ for f in `find . -name ${PAGE_FILE_NAME}`; do
   # convert content from pukiwiki to markdown
   TEMPFILE=$(mktemp -t conv-pkwk2growi.XXXXX) || exit 1
   # see. https://qiita.com/yuki-takei/items/152e20f4421333ae8fd9
-  cat ${f} | sed -e 's/ \[#[0-9a-z]\+\]$//g' -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' -e 's/^---/        -/g' -e 's/^--/    -/g' -e 's/^\(\s*\)-\([^ ]\)/\1- \2/g' -e 's/&br;/<br>/g' -e 's/^#pre{*/```/g' -e 's/^}}*/```/g' -e 's/%%/~~/g' -e "s/^\#lsx/`echo -ne '\u0024'`lsx()/g" > ${TEMPFILE}
+  cat ${f} | sed -e 's/ \[#[0-9a-z]\+\]$//g' \
+    -e 's/^\*\*\*/###/g' -e 's/^\*\*/##/g' -e 's/^\*/#/g' \
+    -e 's/^---/        -/g' -e 's/^--/    -/g' -e 's/^\(\s*\)-\([^ ]\)/\1- \2/g' \
+    -e 's/^[\+][\+][\+]/        1./g' -e 's/^[\+][\+]/    1./g' -e 's/^[\+]/1./g' -e 's/^\(\s*\)1\.\([^ ]\)/\11. \2/g' \
+    -e 's/&br;/<br>/g' \
+    -e 's/^#pre{*/```/g' -e 's/^}}*/```/g' \
+    -e 's/%%/~~/g' \
+    -e "s/^\#lsx/`echo -ne '\u0024'`lsx()/g" \
+    > ${TEMPFILE}
   mv $TEMPFILE ${f}
 done
